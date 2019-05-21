a1fe6f83746d141b4c3e6b6144c1de3729ed567b
anthonyvigz <anthonyvigz@gmail.com>
2019-05-21 16:40:42 -0400

    started filter feature on search page

---
+++ script.js
@@ -0,0 +1,54 @@
+class IconLink {
+    constructor(iconElement) {
+        this.iconElement = iconElement;
+
+        this.iconData = this.iconElement.dataset.icon;
+
+        this.bubbles = document.querySelectorAll(`.kidbubble[data-icon="${this.iconData}"]`);
+
+        this.bubbles = Array.from(this.bubbles).map(bubble => {
+            return new IconBubble(bubble);
+        })
+
+        this.iconElement.addEventListener('click', () => {
+            this.selectIcon();
+        })
+    }
+
+    selectIcon() {
+
+        const icons = document.querySelectorAll('icon');
+
+        icons.forEach(icon => {
+            icon.classList.remove('active-icon');
+        })
+
+        const bubbles = document.querySelectorAll('.kidbubble');
+
+        bubbles.forEach(bubble => {
+            bubble.style.display = 'none';
+        })
+
+        this.iconElement.classList.add('active-icon');
+
+        this.bubbles.forEach(bubble => bubble.selectBubble());
+    }
+}
+
+class IconBubble {
+    constructor(bubbleElement) {
+
+        this.bubbleElement = bubbleElement;
+    }
+
+    selectBubble(){
+
+        this.bubbleElement.style.display = 'flex';
+    }
+}
+
+
+
+const icons = document.querySelectorAll('.icon').forEach(icon => {
+    return new IconLink(icon);
+});
